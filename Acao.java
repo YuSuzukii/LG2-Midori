e9f56d273f23995463aa7bda939ca8927f03ecfa
Minmin <126773140+YuSuzukii@users.noreply.github.com>
2023-09-16 07:54:31 -0300

    Acao.java

---
+++ Acao.java
@@ -0,0 +1,20 @@
+public abstract class Acao {
+
+    private String Nome;
+    private double Preco;
+
+    public Acao(String Nome, double Preco) {
+        this.Nome = nome;
+        this.Preco = preco;
+    }
+
+    public abstract double calcularValor();
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+}
